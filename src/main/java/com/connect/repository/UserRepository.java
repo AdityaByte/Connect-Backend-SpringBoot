b655a4015211e79680d86224f99ad8494addfa39
Aditya-Byte <adityabyte@gmail.com>
2025-06-01 00:24:20 +0530

    repo: user repo class for db tasks

---
+++ src/main/java/com/connect/repository/UserRepository.java
@@ -0,0 +1,36 @@
+package com.connect.repository;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Component;
+
+import com.connect.model.User;
+
+@Component
+public class UserRepository {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    // Repository method for making db queries and interacting with the database.
+
+    public boolean createUser(User user) {
+        return mongoTemplate.insert(user) != null; // It returns the saved user else it returns null.
+    }
+
+    public Optional<User> findByEmail(String email) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("email").is(email));
+        return Optional.ofNullable(mongoTemplate.findOne(query, User.class));
+    }
+
+    public Optional<User> findByUsername(String username) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("username").is(username));
+        return Optional.ofNullable(mongoTemplate.findOne(query, User.class));
+    }
+}
