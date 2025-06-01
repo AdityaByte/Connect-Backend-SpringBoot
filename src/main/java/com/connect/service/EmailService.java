24bb3f236873ae0a2d3b6617356d5200ff44300c
Aditya-Byte <adityapawar4914@gmail.com>
2025-06-02 00:11:38 +0530

    service: implemented email service for sending email and OTPService for generating OTP and validating it

---
+++ src/main/java/com/connect/service/EmailService.java
@@ -0,0 +1,27 @@
+package com.connect.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void sendEmail(String to, String subject, String text) {
+        try {
+            SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+            simpleMailMessage.setTo(to);
+            simpleMailMessage.setSubject(subject);
+            simpleMailMessage.setText(text);
+            javaMailSender.send(simpleMailMessage);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Something went wrong at the server");
+        }
+    }
+
+}
