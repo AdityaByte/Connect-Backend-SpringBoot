30d43f01e35293f7f8beb825ae49894173d89b51
Aditya-Byte <adityabyte@gmail.com>
2025-06-02 00:10:20 +0530

    Model for OTP

---
+++ src/main/java/com/connect/model/OTP.java
@@ -0,0 +1,15 @@
+package com.connect.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OTP {
+    private String otp;
+    private String email;
+}
