28fd3aae85f84551f837458304043f4e1f3cbfd9
Aditya-Byte <adityabyte@gmail.com>
2025-06-13 00:07:11 +0530

    chore(bug&clean): changed the history fetching functionality and also added logs and clean the code

--- src/main/java/com/connect/controller/TokenController.java
+++ src/main/java/com/connect/controller/TokenController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+// Controller for handling the Verify Token Request
+
 @RestController
 @RequestMapping("/api")
 public class TokenController {
