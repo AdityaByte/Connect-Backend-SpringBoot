345556f55cc9974bfcbbd6c4d5b0b3fa0f388932
Aditya-Byte <adityabyte@gmail.com>
2025-06-18 22:24:57 +0530

    feat: Implemented Redis as a Cache Memory for storing data for a certain period of time and clean code

--- src/main/java/com/connect/controller/ChatRoomController.java
+++ src/main/java/com/connect/controller/ChatRoomController.java
@@ -12,7 +12,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 
--- src/main/java/com/connect/service/AuthService.java
+++ src/main/java/com/connect/service/AuthService.java
@@ -5,6 +5,7 @@ import java.util.*;
 import com.connect.dto.LoginUserDTO;
 import com.connect.enums.UserRole;
 import com.connect.enums.UserStatus;
+import com.connect.exception.TimeoutException;
 import com.connect.security.CustomUserDetails;
 import com.connect.utils.JwtUtil;
 import org.slf4j.Logger;
@@ -37,14 +38,18 @@ public class AuthService {
     private EmailService emailService;
 
     @Autowired
+    private RedisService redisService;
+
+    @Autowired
+    private OTPService otpService;
+
+    @Autowired
     private AuthenticationManager authenticationManager;
 
     @Autowired
     private JwtUtil jwtUtil;
 
     private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-
-    private static final Map<String, User> userStore = new HashMap<>();
 
     public void sendOTP(User user) {
         // Checking the User exists previously in the DB or not by Email.
@@ -63,28 +68,28 @@ public class AuthService {
 
         // If the User doesn't exist in the DB.
         // Sending an OTP to the User's email for verifying its profile.
-        String generatedOTP = OTPService.generateOTPForUser(user.getEmail());
+        String generatedOTP = otpService.generateOTPForUser(user.getEmail());
 
         // Sending Email via Email Service class.
         emailService.sendEmail(user.getEmail(), "One Time Password", String
                 .format("Hey %s\nYour OTP for signup is %s\nRegards from Connect", user.getUsername(), generatedOTP));
 
-        // After successfully Sending the Email
-        // For Saving the State of the User for further verification (OTP verification) saving its state
-        // in a persistent storage (in memory).
-        userStore.put(user.getEmail(), user);
+        // Putting the User to the Redis Cache.
+        redisService.cacheUserWithTTL(user);
     }
 
     public void resendOTP(String email) {
         // Fetching the Stored User Data from the persistent storage.
-        User storedUser = userStore.get(email);
+        User storedUser = (User) redisService.getUser(email);
         // Checking the user is the same user in the UserStore.
         // If it's not throwing a Runtime Exception which was further handled by the ControllerAdvice.
         if (storedUser == null) {
             throw new RuntimeException("Email Doesn't exists. Bad Request!");
         }
-
-        String generatedOTP = OTPService.generateOTPForUser(email);
+        // Removing the Pre-cached OTP before creating the Another one if the otp exists.
+        redisService.removeOTP(email);
+        // Generating a new OTP and saving it into the redis cache.
+        String generatedOTP = otpService.generateOTPForUser(email);
         // Resending the OTP to the User's email.
         emailService.sendEmail(storedUser.getEmail(), "One Time Password", String
         .format("Hey %s\nYour OTP for signup is %s\nRegards from Connect", storedUser.getUsername(), generatedOTP));
@@ -93,19 +98,27 @@ public class AuthService {
     public User createUser(String otp, String email) {
         // Before creating the User we need to verify the OTP is valid or not.
         // If the OTP is valid we proceed further else we throw an Exception.
-        if (!OTPService.verifyOTP(email, otp)) {
+        if (!otpService.verifyOTP(email, otp)) {
             throw new RuntimeException("Invalid OTP");
         }
 
+        // Explicitly removing the cache otp if exists in the db.
+        redisService.removeOTP(email);
+
         // Fetching the User from the temporary storage and storing it in the database.
-        User savedUser = userStore.get(email);
+        User savedUser = (User) redisService.getUser(email);
+        if (savedUser == null) {
+            throw new TimeoutException("User not found in the Cache, Try again later");
+        }
         // Before saving it in the DB encoding the password with BCrypt encryption.
         savedUser.setPassword(passwordEncoder.encode(savedUser.getPassword()));
 
         savedUser.setUserRole(List.of(UserRole.USER));
 
         Optional<User> createdUser = repository.createUser(savedUser);
-        userStore.remove(email); // Have to remove it after all.
+
+        // Explicitly removing the cached user.
+        redisService.removeUser(email);
 
         // Returning the User if successfully created else throwing an exception.
         return createdUser
