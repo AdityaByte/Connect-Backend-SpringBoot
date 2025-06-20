2a34c2c993dfb579f641847653eab35b2081660b
Aditya-Byte <adityapawar4914@gmail.com>
2025-06-20 20:58:21 +0530

    chore: added routes and functionality for adding room to db

--- src/main/java/com/connect/controller/ChatRoomController.java
+++ src/main/java/com/connect/controller/ChatRoomController.java
@@ -5,7 +5,10 @@ package com.connect.controller;
 import com.connect.dto.RoomDTO;
 import com.connect.model.Room;
 import com.connect.service.ChatUserService;
+import com.connect.service.JwtTokenService;
 import com.connect.service.RoomService;
+import com.connect.utils.JwtUtil;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,7 +18,6 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 
-import java.security.Principal;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -29,6 +31,9 @@ public class ChatRoomController {
 
     @Autowired
     private RoomService roomService;
+
+    @Autowired
+    private JwtTokenService jwtTokenService;
 
     @Autowired
     private SimpMessagingTemplate messagingTemplate;
@@ -48,13 +53,29 @@ public class ChatRoomController {
     // Handler when the user creates a new room.
     // Person who is creating the room must give the necessary information as in body.
     @PostMapping("/room/create")
-    public ResponseEntity<String> createRoom(@RequestBody  Room room, Principal principal) {
-        // Room name is been given by the owner.
-        // Here we derive the name of the user who is request with the help of principal.
-        Room createdRoom = roomService.addNewRoom(room, principal);
+    public ResponseEntity<Map<String, String>> createRoom(@RequestBody Room room, HttpServletRequest request) {
+        log.info("New Room request");
+        String rawToken = request.getHeader("Authorization");
+        // Checking the token is valid or not.
+        // This service extracts the username till then the token is valid otherwise it will not.
+        String token = rawToken.substring(7);
+        String username = jwtTokenService.extractUsername(token);
+
+        if (username.isEmpty()) {
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .body(Map.of("response", "Invalid Token"));
+        }
+        // Here we need to send the data to the service class.
+        var createdRoom = roomService.addNewRoom(room, username);
+        if (createdRoom == null) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(Map.of("response", "Something went wrong at the server! Try again later."));
+        }
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body("Room created successfully");
+                .body(Map.of("response", createdRoom.getRoomId().toString()));
     }
 
     @GetMapping("/room/getall")
